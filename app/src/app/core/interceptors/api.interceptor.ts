0c68bfb87dca65e3f750418aa5f2fb1f5b909ab1
nntho.hcm <nntho.hcm@vnpt.vn>
2025-04-26 08:25:54 +0700

    trans

--- app/src/app/core/interceptors/api.interceptor.ts
+++
@@ -1,7 +0,0 @@
-import { HttpInterceptorFn } from "@angular/common/http";
-import { environment } from '../../../../environment';
-
-export const apiInterceptor: HttpInterceptorFn = (req, next) => {
-  const apiReq = req.clone({ url: `${environment.apiUrl}${req.url}` });
-  return next(apiReq);
-};
